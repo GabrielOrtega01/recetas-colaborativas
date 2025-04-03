577bfecdef0b973b0a28227e4b5166869d1655a4
nataliaj23 <nataliacopia081@gmail.com>
2025-04-03 12:02:58 +0000

    feat: agrega modelo de receta

--- Receta.java
+++ Receta.java
@@ -1,17 +1,15 @@
 public class Receta {
     private String nombre;
-    private String ingredientes;
-    private String instrucciones; 
-
-    public Receta(String nombre, String ingredientes, String instrucciones) {
+    private String[] ingredientes;
+    private String instrucciones;
+    
+    public Receta(String nombre, String[] ingredientes, String instrucciones) {
         this.nombre = nombre;
         this.ingredientes = ingredientes;
         this.instrucciones = instrucciones;
     }
-
-    public void mostrarReceta() {
-        System.out.println("Receta: " + nombre);
-        System.out.println("Ingredientes: " + ingredientes);
-        System.out.println("Instrucciones: " + instrucciones);
-    }
+    
+    public String getNombre() { return nombre; }
+    public String[] getIngredientes() { return ingredientes; }
+    public String getInstrucciones() { return instrucciones; }
 }
