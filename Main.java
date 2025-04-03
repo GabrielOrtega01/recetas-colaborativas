6a36c0e5dca4acd95843f0958696ac56b51c679b
nataliaj23 <nataliacopia081@gmail.com>
2025-04-03 11:53:02 +0000

    Inicializar proyecto Java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Receta receta = new Receta("Pasta Carbonara", "Pasta, huevos, queso, panceta");
+        Receta receta = new Receta("Pasta Carbonara", "Pasta, huevos, queso, panceta", "Cocinar la pasta, mezclar con huevos, queso y panceta.");
         receta.mostrarReceta();
     }
 }
--- Receta.java
+++ Receta.java
@@ -1,14 +1,17 @@
 public class Receta {
     private String nombre;
     private String ingredientes;
+    private String instrucciones; 
 
-    public Receta(String nombre, String ingredientes) {
+    public Receta(String nombre, String ingredientes, String instrucciones) {
         this.nombre = nombre;
         this.ingredientes = ingredientes;
+        this.instrucciones = instrucciones;
     }
 
     public void mostrarReceta() {
         System.out.println("Receta: " + nombre);
         System.out.println("Ingredientes: " + ingredientes);
+        System.out.println("Instrucciones: " + instrucciones);
     }
 }
